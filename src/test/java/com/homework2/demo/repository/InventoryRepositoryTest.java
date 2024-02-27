e761f7ef7a4469bb877dc9ec79ba754c5f467471
jiahaohe <jiahhe@iu.edu>
2024-02-26 19:08:06 -0500

    i

--- src/test/java/com/homework2/demo/repository/InventoryRepositoryTest.java
+++ src/test/java/com/homework2/demo/repository/InventoryRepositoryTest.java
@@ -40,25 +40,7 @@ class InventoryRepositoryTest {
 
     @Test
     void search() {
-        Guitar guitar1=new Guitar("acd",24,FENDER,"M",ACOUSTIC,BRAZILIAN_ROSEWOOD,ALDER);
-        Guitar guitar2=new Guitar("atd",25,FENDER,"M",ACOUSTIC,BRAZILIAN_ROSEWOOD,ALDER);
-        Guitar guitar3=new Guitar("agd",26,FENDER,"P",ACOUSTIC,BRAZILIAN_ROSEWOOD,ALDER);
-        Guitar guitar4=new Guitar("akd",27,FENDER,"O",ACOUSTIC,BRAZILIAN_ROSEWOOD,ALDER);
 
-        List<Guitar> guitars1=new ArrayList<>();
-        List<Guitar> guitars2=new ArrayList<>();
-        List<Guitar> guitars3=new ArrayList<>();
-        List<Guitar> guitars4=new ArrayList<>();
-
-        guitars1.add(guitar1);
-        guitars2.add(guitar2);
-        guitars3.add(guitar3);
-        guitars4.add(guitar4);
-
-        assertEquals(guitars1,IN.search(new Guitar("acd",24,FENDER,"M",ACOUSTIC,BRAZILIAN_ROSEWOOD,ALDER)));
-        assertEquals(guitars2,IN.search(new Guitar("atd",25,FENDER,"M",ACOUSTIC,BRAZILIAN_ROSEWOOD,ALDER)));
-        assertEquals(guitars3,IN.search(new Guitar("agd",26,FENDER,"P",ACOUSTIC,BRAZILIAN_ROSEWOOD,ALDER)));
-        assertEquals(guitars4,IN.search(new Guitar("akd",27,FENDER,"O",ACOUSTIC,BRAZILIAN_ROSEWOOD,ALDER)));
 
     }
 }
